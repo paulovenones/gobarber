66c35e2a823253e5ca584052ee3f7ebe034c9e2c
Paulo Silva <paulo.venones@gmail.com>
2020-07-16 23:18:58 -0300

    users and files migrations

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'fa42597d6af3f5a2f8cdbde6b65d2826',
+  expiresIn: '7d',
+};
---
+++ src/database/index.js
@@ -0,0 +1,28 @@
+import Sequelize from 'sequelize';
+
+import User from '../app/models/User';
+import File from '../app/models/File';
+
+import databaseConfig from '../config/database';
+
+const models = [User, File];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+    console.log(`conn${this.connection}`);
+    console.log(`conn.models${this.connection.models}`);
+
+    models
+      .map((model) => model.init(this.connection))
+      .map(
+        (model) => model.associate && model.associate(this.connection.models)
+      );
+  }
+}
+
+export default new Database();
