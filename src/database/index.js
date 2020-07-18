f436ad7af338b9b47c15b5d78a14a42c658a1db3
Paulo Silva <paulo.venones@gmail.com>
2020-07-17 23:47:14 -0300

    notfications feature on mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,27 +1,35 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
 import databaseConfig from '../config/database';
 
-const models = [User, File];
+const models = [User, File, Appointment];
 
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
     this.connection = new Sequelize(databaseConfig);
-    console.log(`conn${this.connection}`);
-    console.log(`conn.models${this.connection.models}`);
 
     models
       .map((model) => model.init(this.connection))
       .map(
         (model) => model.associate && model.associate(this.connection.models)
       );
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      { useNewUrlParser: true, useFindAndModify: true }
+    );
   }
 }
 
