2a4338e5ef34be8215b491c700672b2d534e2da2
Paulo Silva <paulo.venones@gmail.com>
2020-07-20 23:13:50 -0300

    send mail witg nodemailer and bee-queue

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '03a62bca39c338',
+    pass: 'ec342ddc0ae16a',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
