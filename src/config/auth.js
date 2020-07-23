883ad10a83a1af81450cfed4e5047cd7d764706b
Paulo Silva <paulo.venones@gmail.com>
2020-07-22 21:19:25 -0300

    first backend release

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: 'fa42597d6af3f5a2f8cdbde6b65d2826',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '03a62bca39c338',
-    pass: 'ec342ddc0ae16a',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
